66598d8f8a6807a2709d9a0ba05308f20b66076b
Raoul Saad <raoul.e.saad@gmail.com>
2026-09-23 14:21:10 +0200

    Add: minimum-stack - submission-4

---
+++ Data Structures & Algorithms/minimum-stack/submission-4.py
@@ -0,0 +1,28 @@
+from collections import deque
+class MinStack:
+
+    def __init__(self):
+        self.stack = deque()
+        self.stack_min = deque()
+
+    def push(self, val: int) -> None:
+        if self.stack_min:
+            self.stack_min.append(min(val,self.stack_min[-1]))
+        else:
+            self.stack_min.append(val)
+        self.stack.append(val)
+        
+
+    def pop(self) -> None:
+        if self.stack:
+            self.stack.pop()
+            self.stack_min.pop()
+        
+
+    def top(self) -> int:
+        if self.stack:
+            return self.stack[-1]
+
+    def getMin(self) -> int:
+        if self.stack_min:
+            return self.stack_min[-1]
